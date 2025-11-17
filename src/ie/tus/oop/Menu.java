0bfa03806fd738d716b672a245eb9e24cbea98f9
sttadic <stjepan.tadich@gmail.com>
2025-11-17 18:04:29 +0000

    Add menu options and handleOptions method

--- src/ie/tus/oop/Menu.java
+++ src/ie/tus/oop/Menu.java
@@ -8,14 +8,38 @@ public class Menu {
 	public void runApplication() {
 		while (keepRunning) {
 			showOptions();
+			keepRunning = false;
+		}
+	}
+
+	private void handleChoice(int choice) {
+		switch (choice) {
+		case 1:
+			/* TODO */;
+		case 2:
+			/* TODO */;
+
 		}
 	}
 
 	private void showOptions() {
 		out.println("************************************************************");
 		out.println("*                                                          *");
-		out.println("*                   Car Rental Manager                     *");
+		out.println("*               Vehicle Rental Management System           *");
 		out.println("*                                                          *");
 		out.println("************************************************************");
+
+		out.println("(1) Display Vehicle Inventory");
+		out.println("(2) Add New Vehicle to Fleet");
+		out.println("(3) Update Vehicle Information");
+		out.println("(4) Remove Vehicle from Fleet");
+		out.println("(5) Process Vehicle Rental");
+		out.println("(6) Process Vehicle Return");
+		out.println("(7) View Active Rental Records");
+		out.println("(8) Exit");
+
+		// Option selection block
+		out.println("");
+		out.println("Select Options (1-8) > ");
 	}
 }
--- src/ie/tus/oop/Runner.java
+++ src/ie/tus/oop/Runner.java
@@ -2,6 +2,6 @@ package ie.tus.oop;
 
 public class Runner {
 	public static void main(String[] args) {
-		new Menu();
+		new Menu().runApplication();
 	}
 }
