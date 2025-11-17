899e9417cc3a7aea243b08eb27ea5536b8de78cc
sttadic <stjepan.tadich@gmail.com>
2025-11-17 19:46:45 +0000

    Update handleChoice to take user input

--- src/ie/tus/oop/Menu.java
+++ src/ie/tus/oop/Menu.java
@@ -2,30 +2,43 @@ package ie.tus.oop;
 
 import static java.lang.System.out;
 
+import java.util.Scanner;
+
 public class Menu {
 	private boolean keepRunning = true;
+	private Scanner scan = new Scanner(System.in);
 
 	public void runApplication() {
 		while (keepRunning) {
 			showOptions();
-			keepRunning = false;
+			handleChoice();
 		}
 	}
 
-	private void handleChoice(int choice) {
+	private void handleChoice() {
+		int choice = -1;
+		try {
+			choice = Integer.parseInt(scan.nextLine());
+		} catch (NumberFormatException e) {
+			out.println("Invalid Entry! Please try again.");
+		}
+
 		switch (choice) {
-		case 1:
-			/* TODO */;
-		case 2:
-			/* TODO */;
+		case 1 -> vehicleInvertory();
+		case 8 -> keepRunning = false;
+		default -> out.println("Invalid Selection! Please try again.");
 
 		}
+	}
+
+	private void vehicleInvertory() {
+
 	}
 
 	private void showOptions() {
 		out.println("************************************************************");
 		out.println("*                                                          *");
-		out.println("*               Vehicle Rental Management System           *");
+		out.println("*             Vehicle Rental Management System             *");
 		out.println("*                                                          *");
 		out.println("************************************************************");
 
